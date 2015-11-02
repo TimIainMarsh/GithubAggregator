ef291430feb4821a783d8930570ac342a815dd24
TimIainMarsh <timiainmarsh@gmail.com>
2015-11-02 22:37:33 +0200

    gets the first 'x' number of usernames

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
+class Users(dict):
+	def __init__(self,lastid):
+		self.lastid = lastid
+
+
 import urllib2
 from bs4 import BeautifulSoup
 import json
 
-page = urllib2.urlopen('https://api.github.com/users/TimIainMarsh').read()
+def readUsers(URL):
+	page = urllib2.urlopen(URL).read()
+	soup = BeautifulSoup(page, "lxml")
+	usersJson = json.loads(soup.get_text())
+	USERS = []
+	for i in usersJson:
+		# print i
+		USERS.append(i['login'])
+	lastid = usersJson[len(usersJson)-1]['id']
+	print(lastid)
+	return USERS, lastid
 
-soup = BeautifulSoup(page, "lxml")
+Users = Users(0)
 
-API = json.loads(soup.get_text())
+while Users.lastid <= 100:
+	startURL = 'https://api.github.com/users?since=' + str(Users.lastid)
+	print startURL
+	USERS,lastid = readUsers(startURL)
+	Users.lastid = lastid
+
+print(Users)
+# print USERS
+# for i in USERS:
+
+
+# for i in USERS:
+# 	print (i)
+# 	print("-----------------------------------------------------------------------------")
+
 
 # print (API)
 
-print(API[u'login'])
-print(API[u'public_repos'])
+# users = User(API[0][u'login'], API[0][u'name'], API)
+
+
+
+# print(API[u'login'])
+# print(API[u'public_repos'])
+
