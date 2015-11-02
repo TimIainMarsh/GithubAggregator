edf117a51eb5e1e66c3d83ec08a0319ea51d4292
TimIainMarsh <timiainmarsh@gmail.com>
2015-11-02 22:53:37 +0200

    gets the first 'x' number of usernames

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def readUsers(URL):
 	usersJson = json.loads(soup.get_text())
 	USERS = []
 	for i in usersJson:
-		# print i
 		USERS.append(i['login'])
 	lastid = usersJson[len(usersJson)-1]['id']
 	print(lastid)
@@ -23,26 +22,10 @@ Users = Users(0)
 
 while Users.lastid <= 100:
 	startURL = 'https://api.github.com/users?since=' + str(Users.lastid)
-	print startURL
 	USERS,lastid = readUsers(startURL)
 	Users.lastid = lastid
 
 print(Users)
-# print USERS
-# for i in USERS:
 
 
-# for i in USERS:
-# 	print (i)
-# 	print("-----------------------------------------------------------------------------")
-
-
-# print (API)
-
-# users = User(API[0][u'login'], API[0][u'name'], API)
-
-
-
-# print(API[u'login'])
-# print(API[u'public_repos'])
 
